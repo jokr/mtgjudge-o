a8ad2d9dccc8e04f60e333b3988d621021a63271
Joel Micha Krebs <joel.krebs@students.unibe.ch>
2014-06-20 19:21:01 +0200

    lots of small styling

--- footer.php
+++ footer.php
@@ -8,15 +8,16 @@
  */
 ?>
 
-	</div><!-- #content -->
+</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'mtgjudge-o' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'mtgjudge-o' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'mtgjudge-o' ), 'o', '<a href="http://www.aleaiactaest.ch" rel="designer">Joel Micha Krebs</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+<footer id="colophon" class="site-footer container" role="contentinfo">
+	<div class="site-info row">
+		<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'mtgjudge-o' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'mtgjudge-o' ), 'WordPress' ); ?></a>
+		<span class="sep"> | </span>
+		<?php printf( __( 'Theme: %1$s by %2$s.', 'mtgjudge-o' ), 'o', '<a href="http://www.aleaiactaest.ch" rel="designer">jokr</a>' ); ?>
+	</div>
+	<!-- .site-info -->
+</footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
--- header.php
+++ header.php
@@ -9,38 +9,44 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title><?php wp_title( '|', true, 'right' ); ?></title>
-<link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title><?php wp_title( '|', true, 'right' ); ?></title>
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
-<?php wp_head(); ?>
+	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="hfeed site container">
-
-	<header id="masthead" class="site-header" role="banner">
+<div id="page" class="hfeed site">
+	<header id="masthead" class="site-header container" role="banner">
 		<div class="row secondary-navigation">
 			<?php wp_nav_menu( array( 'theme_location' => 'secondary' ) ); ?>
 		</div>
 		<div class="site-branding row">
-			<h1 class="site-title col-md-8"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			<h1 class="site-title col-md-8">
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+
 			<h2 class="site-description col-md-4"><?php bloginfo( 'description' ); ?></h2>
 		</div>
+
 		<?php if ( get_header_image() ) : ?>
-			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-				<img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="">
-			</a>
+			<div class="site-header-img row">
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+					<img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="">
+				</a>
+			</div>
 		<?php endif; // End header image check. ?>
 
-		<nav id="site-navigation" class="main-navigation" role="navigation">
+		<nav id="site-navigation" class="main-navigation row" role="navigation">
 			<h1 class="menu-toggle"><?php _e( 'Menu', 'mtgjudge-o' ); ?></h1>
 			<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'mtgjudge-o' ); ?></a>
 
 			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+		</nav>
+		<!-- #site-navigation -->
+	</header>
+	<!-- #masthead -->
 
-	<div id="content" class="site-content row">
+	<div id="content" class="site-content container">
